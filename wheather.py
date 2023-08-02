dc1b4078362ff2bfa4b0d59a462f067f4cfd46e5
alpra <141245469+alpradeveloperstudio@users.noreply.github.com>
2023-08-02 19:39:41 +0500

    wheather.py

--- wheather.py
+++ wheather.py
@@ -3,18 +3,18 @@ from pyowm.utils import config
 from pyowm.utils import timestamps
 import tts
 
-owm = OWM('de07f938cf0d4a14219686e5fc7d77d3')
+owm = OWM('Ваш  API key')
 mgr = owm.weather_manager()
 
 def OWMp():
-	observation = mgr.weather_at_place('Ферма')
+	observation = mgr.weather_at_place('Город')
 	w = observation.weather
 	status = w.detailed_status
 	temp = w.temperature('celsius')['temp']
 	temp1 = w.temperature('celsius')['feels_like']
 	humidity = w.humidity
 	
-	tts.speak('В посёлке ферма сейчас ' + str(status) +
+	tts.speak('В '+ 'Город'+' сейчас ' + str(status) +
 			  '\nТемпература ' + str(round(temp))+ '°c ощущается как '+str(round(temp1))+'°c' +
 			  '\nВлажность ' + str(humidity) + "%" + 
 			  '\nСкорость ветра ' + str(w.wind()['speed']) + " метров в секунду")
